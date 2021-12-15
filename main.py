93d060822c55a7ef4e87e7c82d4a36779b05aca2
Marius am3 <am3marius@gmail.com>
2021-12-15 22:57:05 +0200

    Creation:Class Car

---
+++ main.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self, year="0", model="new", fuel_type="green"):
+        self.year = year
+        self.model = model
+        self.fuel_type = fuel_type
+
+    def drive(self):
+        print("Driving..")
+
+    def park(self):
+        print("Parked..")
+
+    def fill(self):
+        print("Filled..")
+
+    def report(self):
+        print(f"Year:{self.year} | Model:{self.model} | Fuel:{self.fuel_type}")
